bfde0ab0ad2fbf78aa7141b8006880ebdef73bd3
darshil89 <darshilmahraur67@gmail.com>
2023-07-02 09:01:30 +0530

    linked firebase and added getstatic props

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -6,6 +6,7 @@ import classes from './event-item.module.css';
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
+  console.log(image)
 
   const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
     day: 'numeric',
@@ -17,7 +18,7 @@ function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <img src={'/' + image} alt={title} />
+      <img src={image} alt={title} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
